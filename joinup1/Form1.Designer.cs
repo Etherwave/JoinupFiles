0537a5f12f90f5857e5fac8d881e4b84e845f478
unknown <1005682567@qq.com>
2019-01-06 13:20:01 +0800

    first commit

---
+++ joinup1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace joinup1
+{
+    partial class 合并文件夹
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(合并文件夹));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folder1 = new System.Windows.Forms.TextBox();
+            this.folder2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folder_combine = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog3 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件夹1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // folder1
+            // 
+            this.folder1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.folder1.Location = new System.Drawing.Point(83, 83);
+            this.folder1.Name = "folder1";
+            this.folder1.Size = new System.Drawing.Size(477, 36);
+            this.folder1.TabIndex = 1;
+            // 
+            // folder2
+            // 
+            this.folder2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.folder2.Location = new System.Drawing.Point(83, 166);
+            this.folder2.Name = "folder2";
+            this.folder2.Size = new System.Drawing.Size(477, 36);
+            this.folder2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(580, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "选择文件夹2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(245, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(315, 72);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "合并";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(214, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "请选择要合并的文件夹";
+            // 
+            // folder_combine
+            // 
+            this.folder_combine.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.folder_combine.Location = new System.Drawing.Point(83, 233);
+            this.folder_combine.Name = "folder_combine";
+            this.folder_combine.Size = new System.Drawing.Size(477, 36);
+            this.folder_combine.TabIndex = 6;
+            this.folder_combine.Text = "combine";
+            this.folder_combine.TextChanged += new System.EventHandler(this.folder_combine_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(579, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 42);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "合并的新文件夹名称";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // 合并文件夹
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.folder_combine);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.folder2);
+            this.Controls.Add(this.folder1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "合并文件夹";
+            this.Text = "合并文件夹";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.TextBox folder1;
+        private System.Windows.Forms.TextBox folder2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox folder_combine;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog3;
+    }
+}
+
---
+++ joinup1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace joinup1
+{
+    public partial class 合并文件夹 : Form
+    {
+        public string firstToCombineFolder;
+        public string secondToCombineFolder;
+
+        public bool equls(string filePath1, string filePath2)
+        {
+            //创建一个哈希算法对象 
+            using (HashAlgorithm hash = HashAlgorithm.Create())
+            {
+                using (FileStream file1 = new FileStream(filePath1, FileMode.Open), file2 = new FileStream(filePath2, FileMode.Open))
+                {
+                    byte[] hashByte1 = hash.ComputeHash(file1);//哈希算法根据文本得到哈希码的字节数组 
+                    byte[] hashByte2 = hash.ComputeHash(file2);
+                    string str1 = BitConverter.ToString(hashByte1);//将字节数组装换为字符串 
+                    string str2 = BitConverter.ToString(hashByte2);
+                    return (str1 == str2);//比较哈希码 
+                }
+            }
+        }
+
+        public 合并文件夹()
+        {
+            InitializeComponent();
+            this.folder_combine.Text= System.IO.Directory.GetCurrentDirectory()+"\\combine";
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folder1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                folder2.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(this.folder1.Text==""||this.folder2.Text==""||this.folder_combine.Text=="")
+            {
+                this.label1.Text = "请选择要合并的文件夹";
+            }
+            else
+            {
+                this.label1.Text = "正在合并";
+                combine();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog3.ShowDialog() == DialogResult.OK)
+            {
+                folder_combine.Text = folderBrowserDialog3.SelectedPath;
+            }
+        }
+
+        public void CombineFolder(string sourcePath, string destPath)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                if (!Directory.Exists(destPath))
+                {
+                    //目标目录不存在则创建
+                    try
+                    {
+                        Directory.CreateDirectory(destPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("创建目标目录失败:" + ex.Message);
+                    }
+                }
+                //获得源文件下所有文件
+                List<string> files = new List<string>(Directory.GetFiles(sourcePath));
+                //List<string> files_dest = new List<string>(Directory.GetFiles(destPath));
+                files.ForEach(c =>
+                {
+                    string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //覆盖模式
+                    //如果目标文件夹下有该文件，那么首先删除该文件
+                    if (File.Exists(destFile))
+                    {
+                        //如果目标文件夹中有该文件，那么获取两个文件的信息
+                        //如果两个文件不同，那么就将原文件重命名，并把新文件copy过来
+                        FileInfo f1 = new FileInfo(c);
+                        FileInfo f2 = new FileInfo(destFile);
+                        if (f1.Length != f2.Length || !equls(c, destFile))
+                        {
+                            string newPath = destPath + "\\1" + f1.Name;
+                            File.Move(destFile, newPath);
+                            File.Copy(c, destFile);
+                        }
+                    }
+                    else
+                    {
+                        //把新文件复制过来
+                        File.Copy(c, destFile);
+                    }
+                });
+                //获得源文件下所有目录文件
+                List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+
+                folders.ForEach(c =>
+                {
+                    string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //Directory.Move必须要在同一个根目录下移动才有效,不能在不同卷中移动。
+                    //Directory.Move(c, destDir);
+
+                    //采用递归的方法实现
+                    CombineFolder(c, destDir);
+                });
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("源目录不存在!");
+            }
+        }
+
+        public void combine()
+        {
+            //首先创建要合并到的文件夹
+            string Path = this.folder_combine.Text;
+            if (!Directory.Exists(Path))
+            {
+                Directory.CreateDirectory(Path);
+            }
+            /*首先将一个文件夹的全部内容移动到combine文件夹
+             *此处要判断要合并到的文件夹是不是源文件夹
+             * 如果是源文件夹，那么直接将另一个文件夹的内容合并过来就好了
+             * 如果不是那么我们就还需要先将一个文件的内容首先先全部搬过来
+             */
+            //确定合并顺序
+            //firstToCombineFolder首先移进去
+            //secondToCombineFolder第二个移进去
+            this.firstToCombineFolder = this.folder1.Text;
+            this.secondToCombineFolder = this.folder2.Text;
+            if (this.folder_combine.Text == this.folder2.Text)
+            {
+                this.firstToCombineFolder = this.folder2.Text;
+                this.secondToCombineFolder = this.folder1.Text;
+            }
+            //如果firstToCombineFolder等于combine
+            //那么就不需要先将第一个文件先移动进combine文件夹了
+            //否则先将第一个文件夹里的内容合并进combine文件夹
+            if(this.firstToCombineFolder!=this.folder_combine.Text)
+            {
+                this.CombineFolder(this.firstToCombineFolder, this.folder_combine.Text);
+            }
+
+            //将第二个文件夹合并入combine文件夹
+            CombineFolder(this.secondToCombineFolder, this.folder_combine.Text);
+
+            this.label1.Text = "合并完成";
+        }
+
+        private void folder_combine_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
